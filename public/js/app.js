f78539db615bd9ffdb54740324d816593ebceb34
bnazmi <6nazmi@gmail.com>
2018-04-30 00:56:39 +0300

    up-next video , description video , comment video

---
+++ public/js/app.js
@@ -0,0 +1,4 @@
+const select= (selector)=>{
+   return document.querySelector(selector);
+}
+
---
+++ public/js/dom.js
@@ -0,0 +1,4 @@
+
+ select('#sort-menu-icon').addEventListener("click",()=>{
+    select('.comments-sort-menu').classList.toggle('hidden');
+ })
