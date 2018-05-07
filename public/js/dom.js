048a7933cbb93fb3e5361d48e0610b1528ea9008
bnazmi <6nazmi@gmail.com>
2018-05-07 16:35:53 +0300

    omport and upload video

--- public/js/dom.js
+++ public/js/dom.js
@@ -1,48 +1,57 @@
-select("#sort-menu-icon").addEventListener("click", () => {
-  select(".comments-sort-menu").classList.toggle("hidden");
-});
-
-select("#embed-btn").addEventListener("click", () => {
-  select(".embed-text").classList.toggle("hidden");
-});
-
-select("#share-btn").addEventListener("click", () => {
-  select(".share-video").classList.toggle("hidden");
-});
-
-select("#report-btn").addEventListener("click", () => {
-  select(".report-pop").classList.toggle("hidden");
-});
-
-select(".cansle-report").addEventListener("click", () => {
-  select(".report-pop").classList.toggle("hidden");
-});
-
-select("#addto-btn").addEventListener("click", () => {
-  select(".addto-pop").classList.toggle("hidden");
-});
-
-select("#cansle-addto").addEventListener("click", () => {
-  select(".addto-pop").classList.toggle("hidden");
-});
-
-select(".create-playlist-btn").addEventListener("click", () => {
+if(select("#sort-menu-icon")){
+  select("#sort-menu-icon").addEventListener("click", () => {
+    select(".comments-sort-menu").classList.toggle("hidden");
+  });
+  
+  select("#embed-btn").addEventListener("click", () => {
+    select(".embed-text").classList.toggle("hidden");
+  });
+  
+  select("#share-btn").addEventListener("click", () => {
+    select(".share-video").classList.toggle("hidden");
+  });
+  
+  select("#report-btn").addEventListener("click", () => {
+    select(".report-pop").classList.toggle("hidden");
+  });
+  
+  select(".cansle-report").addEventListener("click", () => {
+    select(".report-pop").classList.toggle("hidden");
+  });
+  
+  select("#addto-btn").addEventListener("click", () => {
     select(".addto-pop").classList.toggle("hidden");
-    select(".create-playlist-pop").classList.toggle("hidden");
   });
-
-select(".cansle-create-playlist").addEventListener("click", () => {
-    select(".create-playlist-pop").classList.toggle("hidden");
+  
+  select("#cansle-addto").addEventListener("click", () => {
+    select(".addto-pop").classList.toggle("hidden");
   });
+  
+  select(".create-playlist-btn").addEventListener("click", () => {
+      select(".addto-pop").classList.toggle("hidden");
+      select(".create-playlist-pop").classList.toggle("hidden");
+    });
+  
+  select(".cansle-create-playlist").addEventListener("click", () => {
+      select(".create-playlist-pop").classList.toggle("hidden");
+    });
+  
+  select(".video-show-description").addEventListener("click", () => {
+    if (select(".video-show-description").textContent.trim() === "Show more") {
+      select(".watch-video-description").style.maxHeight = "100%";
+      select(".video-show-description").textContent = "Show less";
+    } else if (
+      select(".video-show-description").textContent.trim() === "Show less"
+    ) {
+      select(".watch-video-description").style.maxHeight = "100px";
+      select(".video-show-description").textContent = "Show more";
+    }
+  });
+  
 
-select(".video-show-description").addEventListener("click", () => {
-  if (select(".video-show-description").textContent.trim() === "Show more") {
-    select(".watch-video-description").style.maxHeight = "100%";
-    select(".video-show-description").textContent = "Show less";
-  } else if (
-    select(".video-show-description").textContent.trim() === "Show less"
-  ) {
-    select(".watch-video-description").style.maxHeight = "100px";
-    select(".video-show-description").textContent = "Show more";
-  }
+}
+
+select(".upload-btn").addEventListener("click", () => {
+  select(".upload--video").classList.toggle("hidden");
+  select(".upload--video-form").classList.toggle("hidden");
 });
