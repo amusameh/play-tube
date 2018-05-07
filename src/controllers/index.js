fb561e4e91be00165d9a9929f0013cd88b9371c6
Ahmed Shatat <a.shatat@hotmail.com>
2018-05-07 15:10:42 +0300

    start: import hbs

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const home = require('./home');
 const register = require('./register');
+const importvideo = require('./import');
 
 router.get('/', home.get);
 router.get('/register', register.get);
 router.post('/register', register.post);
+router.get('/import', importvideo.get);
 
 module.exports = router;
